62339db210eb7427d64bd5324098a25f1b995aa1
Casey Slaught <c.slaught@keystone.africa>
2019-06-19 16:37:59 +0200

    url hidden

--- account/urls.py
+++ account/urls.py
@@ -3,15 +3,15 @@ from django.urls import include, path
 from account.views import password, outputs, profile, register, tokens
 
 urlpatterns = [
-    path('forced_password_reset/', password.ForcedPasswordResetView.as_view()),
+    #path('forced_password_reset/', password.ForcedPasswordResetView.as_view()),
     path('forgot_password/', password.ForgotPasswordView.as_view()),
     path('forgot_password_confirm/', password.ForgotPasswordConfirmView.as_view()),
     path('get_profile/', profile.GetProfileView.as_view()),
-    path('get_kmz_hrefs/', outputs.GetKmzHrefsView.as_view()),
+    #path('get_kmz_hrefs/', outputs.GetKmzHrefsView.as_view()),
     path('login/', tokens.LoginView.as_view()),
     path('logout/', tokens.LogoutView.as_view()),
     path('refresh/', tokens.RefreshView.as_view()),
     path('register/', register.RegisterView.as_view()),
-    path('update_account/', profile.UpdateAccountView.as_view()),
-    path('update_organization/', profile.UpdateOrganizationView.as_view()),
+    #path('update_account/', profile.UpdateAccountView.as_view()),
+    #path('update_organization/', profile.UpdateOrganizationView.as_view()),
 ]
--- caracal/urls.py
+++ caracal/urls.py
@@ -17,10 +17,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('account/', include('account.urls')),
-    path('activity/', include('activity.urls')),
+    #path('activity/', include('activity.urls')),
     path('admin/', admin.site.urls),
-    path('collars/', include('collars.urls')),
-    path('oauth/', include('oauth.urls')),
-    path('public/', include('public.urls')),
+    #path('collars/', include('collars.urls')),
+    #path('oauth/', include('oauth.urls')),
+    #path('public/', include('public.urls')),
     re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
