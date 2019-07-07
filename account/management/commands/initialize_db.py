e2faafda91bc93b8d82012b4b18a7faaab09f88b
Casey Slaught <casey@quip.link>
2019-07-07 17:22:19 +0200

    initialize db code refactoring

--- account/management/commands/initialize_db.py
+++ account/management/commands/initialize_db.py
@@ -44,55 +44,8 @@ class Command(BaseCommand):
         superuser = Account.objects.create_superuser(os.environ['CARACAL_OVERLORD_EMAIL'],
                                                      os.environ['CARACAL_OVERLORD_PASSWORD'])
 
-        # add dummy alerts
-        alerts = [
-            {
-                'message': 'elephant Martin has not moved in 16 hours',
-                'level': 'high'
-            },
-            {
-                'message': 'elephant Marie has not moved in 8 hours',
-                'level': 'high'
-            },
-            {
-                'message': 'elephant Bosco has travel 42.5 km in the last 24 hours',
-                'level': 'medium'
-            },
-            {
-                'message': 'elephant James has travel 51.2 km in the last 24 hours',
-                'level': 'medium'
-            },
-            {
-                'message': 'no radio positions have been received in the last 12 hours',
-                'level': 'low'
-            },
-            {
-                'message': 'no Orbcomm collar positions have been received in the last 24 hours',
-                'level': 'low'
-            }
-        ]
-
-        for alert in alerts:
-            ActivityAlert.objects.create(organization=superuser.organization, **alert)
-
-        # add dummy changes
-        changes = [
-            {
-                'message': 'Martin Ishimwe added an elephant collar account'
-            },
-            {
-                'message': 'Roger Green dismissed alert (elephant Dumbo has travel 36.2 km in the last 24 hours)'
-            },
-            {
-                'message': 'Martin Ishimwe added connected an ArcGIS Online account'
-            },
-            {
-                'message': 'Roger Green added connected a Google Drive account'
-            }
-        ]
-
-        for change in changes:
-            ActivityChange.objects.create(organization=superuser.organization, account=superuser, **change)
+        common.add_dummy_alerts(superuser)
+        common.add_dummy_changes(superuser)
 
 
 
--- account/management/commands/utils/common.py
+++ account/management/commands/utils/common.py
@@ -8,6 +8,60 @@ from auth import cognito
 from caracal.common.models import RealTimeAccount, RealTimeIndividual, RealTimePosition, RealTimePositionHash
 
 
+def add_dummy_alerts(account):
+    print('...adding dummy alerts')
+    alerts = [
+        {
+            'message': 'elephant Martin has not moved in 16 hours',
+            'level': 'high'
+        },
+        {
+            'message': 'elephant Marie has not moved in 8 hours',
+            'level': 'high'
+        },
+        {
+            'message': 'elephant Bosco has travel 42.5 km in the last 24 hours',
+            'level': 'medium'
+        },
+        {
+            'message': 'elephant James has travel 51.2 km in the last 24 hours',
+            'level': 'medium'
+        },
+        {
+            'message': 'no radio positions have been received in the last 12 hours',
+            'level': 'low'
+        },
+        {
+            'message': 'no Orbcomm collar positions have been received in the last 24 hours',
+            'level': 'low'
+        }
+    ]
+
+    for alert in alerts:
+        ActivityAlert.objects.create(organization=account.organization, **alert)
+
+
+def add_dummy_changes(account):
+    print('...adding dummy changes')
+    changes = [
+        {
+            'message': 'Martin Ishimwe added an elephant collar account'
+        },
+        {
+            'message': 'Roger Green dismissed alert (elephant Dumbo has travel 36.2 km in the last 24 hours)'
+        },
+        {
+            'message': 'Martin Ishimwe added connected an ArcGIS Online account'
+        },
+        {
+            'message': 'Roger Green added connected a Google Drive account'
+        }
+    ]
+
+    for change in changes:
+        ActivityChange.objects.create(organization=account.organization, account=account, **change)
+
+
 def clear_all_content():
     print("...clearing all content")
     ActivityAlert.objects.all().delete()
